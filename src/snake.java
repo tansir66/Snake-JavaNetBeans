f83bef9a317a55775bc9ea892613653c793bf7f5
tansir33@gmail.com <74235426+tansir66@users.noreply.github.com>
2022-09-27 11:11:56 +0600

    Add files via upload

---
+++ src/snake.java
@@ -0,0 +1,28 @@
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class snake extends JFrame {
+    public snake(){
+        initUI(); 
+    }
+
+    private void initUI() {
+        add(new Board());   
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake Game");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() ->{
+        JFrame ex = new snake();       
+        ex.setVisible(true);
+        });
+    }
+    
+}
